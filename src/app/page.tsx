e38412aeb6407dcfc14de716a7deb798922531be
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-05 16:30:03 +0600

    first button done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+<main className="flex min-h-screen items-center justify-center bg-gray-100">
+  <button className="relative group focus:outline-none active:scale-95 transition w-[80vw] max-w-96 aspect-square">
+    {/* Outer circle */}
+    <div className="absolute inset-0 bg-sky-200 rounded-full group-hover:bg-sky-300"></div>
+
+    {/* Inner circle */}
+    <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
+      {/* Icon */}
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+      </svg>
+      {/* Text */}
+      <span className="text-sm md:text-2xl">Social <br></br> Protection</span>
+    </div>
+  </button>
+
+
+  {/* 2nd button */}
+  
+</main>
+
+
+
   );
 }
