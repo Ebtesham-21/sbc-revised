5401f98c408d39a6c23e0c759c4f8e269d4f1893
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-08 16:50:19 +0600

    what is sbc corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
 <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 space-y-8 p-6  space-x-6">
 
 {/* sbc button */}
-<button className="relative w-[90vw] max-w-[900px] aspect-[8/1] rounded-full focus:outline-none active:scale-95 transition group">
+<button className="relative w-[90vw] max-w-[800px] aspect-[8/1] rounded-full focus:outline-none active:scale-95 transition group">
   {/* Outer Layer */}
-  <div className="absolute inset-0 bg-amber-400 rounded-full group-hover:bg-amber-300"></div>
+  <div className="absolute inset-x-[2%] inset-y-[-10%] bg-amber-400 rounded-full group-hover:bg-amber-300"></div>
 
   {/* Middle Layer */}
-  <div className="absolute inset-x-[3%] inset-y-[6%] bg-white rounded-full"></div>
+  <div className="absolute inset-x-[4%] inset-y-[4%] bg-white rounded-full"></div>
 
   {/* Inner Layer */}
   <div className="absolute inset-x-[5%] inset-y-[10%] bg-sky-500 rounded-full group-hover:bg-sky-600"></div>
