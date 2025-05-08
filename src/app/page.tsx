a7b54dfcc959b79228de08d1cb8193acf9233981
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-08 12:21:04 +0600

    audio files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -396,7 +396,213 @@ export default function Home() {
     </div>
   </button>
 
+{/* House  Icon */}
 
+<button className="relative w-[80vw] max-w-[700px] aspect-[1/1] focus:outline-none active:scale-95 transition group">
+    {/* Outer rectangle */}
+    <div className="absolute inset-0 bg-sky-500 rounded-[7%] group-hover:bg-sky-400"></div>
+
+    {/* Inner rectangle */}
+    <div className="absolute left-[4%] top-[4%] w-[92%] h-[92%] bg-blue-800 rounded-[7%] group-hover:bg-blue-700"></div>
+
+    {/* Outer circle */}
+    <div className="absolute left-[12%] top-[11%] w-[76%] h-[78%] bg-sky-500 rounded-full group-hover:bg-sky-400"></div>
+
+    {/* Inner circle with content */}
+    <div className="absolute left-[16%] top-[15%] w-[68%] h-[70%] bg-blue-800 rounded-full group-hover:bg-blue-700 flex flex-col items-center justify-center text-white text-base md:text-xl font-semibold space-y-2">
+      
+      <div className="absolute inset-0 flex items-center justify-center text-white font-semibold text-sm sm:text-base md:text-xl lg:text-9xl text-center px-4">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-56 md:w-56" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 10l9-7 9 7v10a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V14H9v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V10z" />
+      </svg>
+
+    </div>
+      
+      
+    </div>
+  </button>
+
+
+{/* Explore button */}
+ 
+<button className="relative w-[90vw] max-w-[1090px] aspect-[4/1] rounded-[203px] focus:outline-none active:scale-95 transition group m-2">
+  {/* Outer layer */}
+  <div className="absolute inset-0 bg-sky-200 rounded-[203px] group-hover:bg-sky-300"></div>
+
+  {/* Middle layer */}
+  <div className="absolute inset-[8%] bg-white rounded-[203px]"></div>
+
+  {/* Inner layer */}
+  <div className="absolute inset-[12%] bg-sky-500 rounded-[203px] group-hover:bg-sky-600"></div>
+
+  {/* Text */}
+  <div className="absolute inset-0 flex items-center justify-center text-white font-semibold text-xl md:text-3xl text-center">
+  
+    <span>Explore</span>
+  </div>
+</button>
+
+
+{/* Early Childhood Development */}
+<button className="relative group focus:outline-none active:scale-95 transition w-[80vw] max-w-96 aspect-square m-4">
+    {/* Outer circle */}
+    <div className="absolute inset-0 bg-sky-200 rounded-full group-hover:bg-sky-300"></div>
+
+    {/* Inner circle */}
+    <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
+      {/* Icon */}
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+      </svg>
+      {/* Text */}
+      <span className="text-sm md:text-2xl">Early Childhood <br></br> Development </span>
+    </div>
+  </button>
+
+
+
+{/* !  Icon */}
+
+<button className="relative w-[80vw] max-w-[700px] aspect-[1/1] focus:outline-none active:scale-95 transition group">
+    {/* Outer rectangle */}
+    <div className="absolute inset-0 bg-sky-500 rounded-[7%] group-hover:bg-sky-400"></div>
+
+    {/* Inner rectangle */}
+    <div className="absolute left-[4%] top-[4%] w-[92%] h-[92%] bg-blue-800 rounded-[7%] group-hover:bg-blue-700"></div>
+
+    {/* Outer circle */}
+    <div className="absolute left-[12%] top-[11%] w-[76%] h-[78%] bg-sky-500 rounded-full group-hover:bg-sky-400"></div>
+
+    {/* Inner circle with content */}
+    <div className="absolute left-[16%] top-[15%] w-[68%] h-[70%] bg-blue-800 rounded-full group-hover:bg-blue-700 flex flex-col items-center justify-center text-white text-base md:text-xl font-semibold space-y-2">
+      
+      <div className="absolute inset-0 flex items-center justify-center text-white font-semibold text-sm sm:text-base md:text-xl lg:text-9xl text-center px-4">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-80 md:w-80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v4m0 4h.01" />
+      </svg>
+
+
+    </div>
+      
+      
+    </div>
+  </button>
+
+
+{/* Early Childhood Development */}
+<button className="relative group focus:outline-none active:scale-95 transition w-[80vw] max-w-96 aspect-square m-4">
+    {/* Outer circle */}
+    <div className="absolute inset-0 bg-sky-200 rounded-full group-hover:bg-sky-300"></div>
+
+    {/* Inner circle */}
+    <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
+      {/* Icon */}
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+      </svg>
+      {/* Text */}
+      <span className="text-sm md:text-2xl">Climate <br></br> Change </span>
+    </div>
+  </button>
+
+
+{/* Measles */}
+  <button className="relative group focus:outline-none active:scale-95 transition w-[80vw] max-w-96 aspect-square m-4">
+    {/* Outer circle */}
+    <div className="absolute inset-0 bg-sky-200 rounded-full group-hover:bg-sky-300"></div>
+
+    {/* Inner circle */}
+    <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
+     
+      {/* Text */}
+      <span className="text-sm md:text-4xl">Cholera<br></br> </span>
+    </div>
+  </button>
+
+
+  {/* Child Survival and Development */}
+<button className="relative group focus:outline-none active:scale-95 transition w-[80vw] max-w-96 aspect-square m-4">
+    {/* Outer circle */}
+    <div className="absolute inset-0 bg-sky-200 rounded-full group-hover:bg-sky-300"></div>
+
+    {/* Inner circle */}
+    <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
+      {/* Icon */}
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+      </svg>
+      {/* Text */}
+      <span className="text-sm md:text-2xl">Child Survival <br></br> And <br></br> Development </span>
+    </div>
+  </button>
+
+  {/* Child Rights */}
+<button className="relative group focus:outline-none active:scale-95 transition w-[80vw] max-w-96 aspect-square m-4">
+    {/* Outer circle */}
+    <div className="absolute inset-0 bg-sky-200 rounded-full group-hover:bg-sky-300"></div>
+
+    {/* Inner circle */}
+    <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
+      {/* Icon */}
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+      </svg>
+      {/* Text */}
+      <span className="text-sm md:text-2xl">Child  <br></br> Rights <br></br>  </span>
+    </div>
+  </button>
+
+
+{/* !  Left Arrow */}
+
+<button className="relative w-[80vw] max-w-[700px] aspect-[1/1] focus:outline-none active:scale-95 transition group">
+    {/* Outer rectangle */}
+    <div className="absolute inset-0 bg-sky-500 rounded-[7%] group-hover:bg-sky-400"></div>
+
+    {/* Inner rectangle */}
+    <div className="absolute left-[4%] top-[4%] w-[92%] h-[92%] bg-blue-800 rounded-[7%] group-hover:bg-blue-700"></div>
+
+    {/* Outer circle */}
+    <div className="absolute left-[12%] top-[11%] w-[76%] h-[78%] bg-sky-500 rounded-full group-hover:bg-sky-400"></div>
+
+    {/* Inner circle with content */}
+    <div className="absolute left-[16%] top-[15%] w-[68%] h-[70%] bg-blue-800 rounded-full group-hover:bg-blue-700 flex flex-col items-center justify-center text-white text-base md:text-xl font-semibold space-y-2">
+      
+      <div className="absolute inset-0 flex items-center justify-center text-white font-semibold text-sm sm:text-base md:text-xl lg:text-9xl text-center px-4">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-80 md:w-80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+      </svg>
+
+
+
+    </div>
+      
+      
+    </div>
+  </button>
+
+
+  {/* Audio  Files */}
+ 
+<button className="relative w-[90vw] max-w-[1090px] aspect-[4/1] rounded-[203px] focus:outline-none active:scale-95 transition group m-2">
+  {/* Outer layer */}
+  <div className="absolute inset-0 bg-sky-200 rounded-[203px] group-hover:bg-sky-300"></div>
+
+  {/* Middle layer */}
+  <div className="absolute inset-[8%] bg-white rounded-[203px]"></div>
+
+  {/* Inner layer */}
+  <div className="absolute inset-[12%] bg-sky-500 rounded-[203px] group-hover:bg-sky-600"></div>
+
+  {/* Text */}
+  <div className="absolute inset-0 flex items-center justify-center text-white font-semibold text-xl md:text-3xl text-center">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-40 md:w-40" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M9 17.5a2.5 2.5 0 1 1-2-2.45V6.09l10-2.18v10.14a2.5 2.5 0 1 1-2 2.45 2.5 2.5 0 0 1 2-2.45V6.91l-6 1.31v7.83a2.5 2.5 0 1 1-2 2.45z"/>
+  </svg>
+
+    <span>Audio Files</span>
+  </div>
+</button>
 
 
 
