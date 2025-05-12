78c506c80b3e34af41e5bde5c9537d76d99b8805
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-12 18:27:30 +0600

    document files issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,11 +191,11 @@ export default function Home() {
     {/* Inner circle */}
     <div className="absolute inset-0 m-auto w-[90%] aspect-square bg-sky-500 rounded-full border-[6px] border-white group-hover:bg-sky-600 flex flex-col items-center justify-center text-white font-semibold space-y-2">
       {/* Icon */}
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 md:h-20 md:w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
       </svg>
       {/* Text */}
-      <span className="text-sm md:text-2xl">Out of school <br></br> Children</span>
+      <span className="text-xl md:text-2xl">Out of school <br></br> Children</span>
     </div>
   </button>
 
